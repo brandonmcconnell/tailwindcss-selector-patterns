56ca4d9d46709cb2c5a41e7c0a6392fdf2bad74b
Brandon McConnell <brandon@dreamthinkbuild.com>
2024-04-21 23:27:39 -0700

    Fix exports

--- dist/esm/index.js
+++ dist/esm/index.js
@@ -1,27 +1,23 @@
 import plugin from 'tailwindcss/plugin';
-export default plugin(({ matchVariant }) => {
-    matchVariant("pattern", (pattern_selector) => {
-        const [pattern, selector = "*", extraneous] = pattern_selector
-            .split(";")
-            .map((str) => str.trim());
-        const optionalSelector = selector === "*" ? "" : selector;
+module.exports = plugin(({ matchVariant }) => {
+    matchVariant('pattern', (pattern_selector) => {
+        const [pattern, selector = '*', extraneous] = pattern_selector.split(';').map((str) => str.trim());
+        const optionalSelector = selector === '*' ? '' : selector;
         if (extraneous)
             throw new Error(`Found more than 2 semicolon-delimited (;) arguments in Tailwind CSS selector pattern '${pattern_selector}'`);
         const combinator = pattern[0];
-        const validCombinatorSymbols = ["+", "-", "%", ">", "<", "^"];
+        const validCombinatorSymbols = ['+', '-', '%', '>', '<', '^'];
         if (!validCombinatorSymbols.includes(combinator))
-            throw new Error(`Combinator used '${combinator}' in Tailwind CSS selector pattern '${pattern_selector}' is not one of the valid combinators: ${validCombinatorSymbols.join(", ")}`);
+            throw new Error(`Combinator used '${combinator}' in Tailwind CSS selector pattern '${pattern_selector}' is not one of the valid combinators: ${validCombinatorSymbols.join(', ')}`);
         const isDoubleCombinator = pattern[1] === combinator;
         if (!isDoubleCombinator && validCombinatorSymbols.includes(pattern[1]))
             throw new Error(`Second character in Tailwind CSS selector pattern '${pattern_selector}' ('${pattern[1]}') is a valid combinator but does not match the first combinator used ('${combinator}')`);
-        const nthIndex_dir = isDoubleCombinator
-            ? pattern.slice(2)
-            : pattern.slice(1);
+        const nthIndex_dir = isDoubleCombinator ? pattern.slice(2) : pattern.slice(1);
         if (isDoubleCombinator && nthIndex_dir)
             throw new Error(`Double combinator '${combinator.repeat(2)}' in Tailwind CSS selector pattern '${pattern_selector}' can only be followed by an optional selector argument, delimited by a semicolon; instead found '${nthIndex_dir}'`);
-        const validDirSymbols = ["<", ">"];
+        const validDirSymbols = ['<', '>'];
         const hasDir = !isDoubleCombinator && validDirSymbols.includes(nthIndex_dir.slice(-1));
-        const [_nthIndex = "1", dir] = !isDoubleCombinator
+        const [_nthIndex = '1', dir] = !isDoubleCombinator
             ? hasDir
                 ? [nthIndex_dir.slice(0, -1), nthIndex_dir.slice(-1)]
                 : nthIndex_dir
@@ -40,73 +36,65 @@ export default plugin(({ matchVariant }) => {
             throw new Error(`Invalid nth-index value '${_nthIndex}' in Tailwind CSS selector pattern '${pattern_selector}'`);
         if (!(nthIndex > 0))
             throw new Error(`nth-index value '${nthIndex}' in Tailwind CSS selector pattern '${pattern_selector}' must be greater than 0`);
-        let result = "";
-        if (combinator === "+" || combinator === "%") {
+        let result = '';
+        if (combinator === '+' || combinator === '%') {
             if (isDoubleCombinator) {
                 result += `& ~ ${selector}`;
             }
             else if (!dir) {
-                result += `& + ${"* + ".repeat(nthIndex - 1)}${selector}`;
+                result += `& + ${'* + '.repeat(nthIndex - 1)}${selector}`;
             }
-            else if (dir === "<") {
-                result += [...Array(nthIndex)]
-                    .map((_, i) => `& + ${"* + ".repeat(i)}${selector}`)
-                    .join(", ");
+            else if (dir === '<') {
+                result += [...Array(nthIndex)].map((_, i) => `& + ${'* + '.repeat(i)}${selector}`).join(', ');
             }
-            else if (dir === ">") {
-                result += `& ${"+ * ".repeat(nthIndex - 1)}~ ${selector}`;
+            else if (dir === '>') {
+                result += `& ${'+ * '.repeat(nthIndex - 1)}~ ${selector}`;
             }
         }
-        if (combinator === "%")
+        if (combinator === '%')
             result += `, `;
-        if (combinator === "-" || combinator === "%") {
+        if (combinator === '-' || combinator === '%') {
             if (isDoubleCombinator) {
                 result += `${optionalSelector}:has(~ &)`;
             }
             else if (!dir) {
-                result += `${optionalSelector}:has(${"+ * ".repeat(nthIndex - 1)}+ &)`;
+                result += `${optionalSelector}:has(${'+ * '.repeat(nthIndex - 1)}+ &)`;
             }
-            else if (dir === "<") {
-                result += `${optionalSelector}:has(${[...Array(nthIndex)]
-                    .map((_, i) => `+ ${"* + ".repeat(i)}&`)
-                    .join(", ")})`;
+            else if (dir === '<') {
+                result += `${optionalSelector}:has(${[...Array(nthIndex)].map((_, i) => `+ ${'* + '.repeat(i)}&`).join(', ')})`;
             }
-            else if (dir === ">") {
-                result += `${optionalSelector}:has(~${" * +".repeat(nthIndex - 1)} &)`;
+            else if (dir === '>') {
+                result += `${optionalSelector}:has(~${' * +'.repeat(nthIndex - 1)} &)`;
             }
         }
-        if (combinator === ">" || combinator === "^") {
+        if (combinator === '>' || combinator === '^') {
             if (isDoubleCombinator) {
                 result += `& ${selector}`;
             }
             else if (!dir) {
-                result += `& ${"> * ".repeat(nthIndex - 1)}> ${selector}`;
+                result += `& ${'> * '.repeat(nthIndex - 1)}> ${selector}`;
             }
-            else if (dir === "<") {
-                result += [...Array(nthIndex)]
-                    .map((_, i) => `& > ${"* > ".repeat(i)}${selector}`)
-                    .join(", ");
+            else if (dir === '<') {
+                result += [...Array(nthIndex)].map((_, i) => `& > ${'* > '.repeat(i)}${selector}`).join(', ');
             }
-            else if (dir === ">") {
-                result += `& ${"> * ".repeat(nthIndex - 1)}${selector}`;
+            else if (dir === '>') {
+                result += `& ${'> * '.repeat(nthIndex - 1)}${selector}`;
             }
         }
-        if (combinator === "^")
+        if (combinator === '^')
             result += `, `;
-        if (combinator === "<" || combinator === "^") {
+        if (combinator === '<' || combinator === '^') {
             if (isDoubleCombinator) {
                 result += `${optionalSelector}:has(&)`;
             }
             else if (!dir) {
-                result += `${optionalSelector}:has(${"> * ".repeat(nthIndex - 1)}> &)`;
+                result += `${optionalSelector}:has(${'> * '.repeat(nthIndex - 1)}> &)`;
             }
-            else if (dir === "<") {
-                result += `${optionalSelector}:has(${[...Array(nthIndex)]
-                    .map((_, i) => `${"> * ".repeat(i)}> &`)
-                    .join(", ")})`;
+            else if (dir === '<') {
+                result += `${optionalSelector}:has(${[...Array(nthIndex)].map((_, i) => `${'> * '.repeat(i)}> &`).join(', ')})`;
             }
-            else if (dir === ">") {
-                result += `${optionalSelector}:has(${"* > ".repeat(nthIndex - 1)}&)`;
+            else if (dir === '>') {
+                result += `${optionalSelector}:has(${'* > '.repeat(nthIndex - 1)}&)`;
             }
         }
         return `:is(${result})`;
